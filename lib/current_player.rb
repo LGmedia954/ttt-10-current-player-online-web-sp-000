3660b2756e0398aa0cd88334dc7ee755dd01b47e
Leslie G <lesliegonzalez@me.com>
2020-06-16 19:02:30 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 def turn_count(board)
   counter = 0
   board.each do |index|
-  if index == "X" || "O"
+  if index == "X" || index == "O"
   counter += 1
   end
 end
