92fa176c8b3367cbf833ce4ac629254de9b2d6e0
Leslie G <lesliegonzalez@me.com>
2020-06-16 19:00:59 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,12 @@
 board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-def turn_count do
+def turn_count(baord)
   counter = 0
   board.each do |index|
   if index == "X" || "O"
   counter += 1
   end
 end
-  return turn_count
+  return turn_count(board)
 end
   
