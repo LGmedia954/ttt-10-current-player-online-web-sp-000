d09549f2cfe7bd20587af17e243c2ab53458b6b7
Leslie G <lesliegonzalez@me.com>
2020-06-16 19:52:26 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,10 +11,10 @@ end
 end
 
 def current_player(board)
-  if current_player(board) == X
+  if current_player(board) == X && counter == 0
   return O
 end
-  if current_player(board) == O
+  if current_player(board) == O && counter == 1
   return X
 end
   if counter % 3 == true
